f441cc31a2e46f11affa572e33d26206ad0e297e
MustafaArif519 <mustafaarif519@gmail.com>
2023-03-03 17:57:01 -0500

    Reorganized app structure for scalability

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from . import db
+# defines the schema of the sql databases
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class Cake(db.Model):
+    __tablename__ = 'cakes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128))
+    file = db.Column(db.String(128), unique=True)
+
+    def __repr__(self):
+        return '<Cake %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
